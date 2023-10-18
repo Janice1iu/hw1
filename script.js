acb85813b230514d92a560fe078ac94a3d5a81c3
janice <janiceliu26@gmail.com>
2023-10-19 00:07:40 +0800

    update

--- script.js
+++ script.js
@@ -43,55 +43,25 @@ const align = () => {
   const button = document.getElementById("task3-1-button");
   const columns = document.getElementsByClassName("task3-1-column");
 
-  // Define an array of all 3 alignments 
+  // Define an array of alignment classes
   const alignClasses = ["text-start", "text-center", "text-end"];
-  let alignIndex = 0; // Current alignment index
+  let alignIndex = 0; 
 
-  const applyAlignment = () => {
+  button.addEventListener("click", () => {
     for (let i = 0; i < columns.length; i++) {
       const column = columns[i];
       column.classList.remove(...alignClasses); // Remove all alignment classes from the column
       column.classList.add(alignClasses[alignIndex]); // Add current alignment class to the column
     }
-  };
 
-  const handleClick = () => {
-    applyAlignment();
-    // Increment the alignment index and wrap around after 3
+    // Increment the alignment index and wrap around if necessary
     alignIndex = (alignIndex + 1) % alignClasses.length;
-  };
-
-  button.addEventListener("click", handleClick);
-
-  // Apply initial alignment when the page loads
-  applyAlignment();
-
-  // Apply alignment on window load event as well
-  window.addEventListener("load", handleClick);
+  });
 };
 
 align();
-/*
+
 // Task 3.2
-const handleSpotlight = () => {
-  const button = document.getElementById("task3-2-button");
-
-  button.addEventListener("click", () => {
-    const input = prompt("Enter a spotlight of Chihara Junior");
-    alert(`The spotlight you input is: ${input}`);
-
-    if (input !== null && input.trim() !== "") {
-      const spotlightColumn = document.getElementById('spotlightColumn');
-      const spotlightBox = document.createElement('div');
-      spotlightBox.className = 'spotlightBox container container-gradient';
-      spotlightBox.textContent = input;
-      spotlightColumn.appendChild(spotlightBox);
-    }
-  });
-}; 
-
-handleSpotlight();*/
-
 const handleSpotlight = () => {
   const button = document.getElementById("task3-2-button");
 
@@ -111,11 +81,11 @@ const handleSpotlight = () => {
 
        // Apply responsive class based on text length
        if (input.length > 100) {
-        spotlightColumn.classList.add('col-12'); // Set column to full width for long text
+        spotlightColumn.classList.add('col-12'); // full width for long text
       } else if (input.length > 50) {
-        spotlightColumn.classList.add('col-md-6'); // Set column to half width on medium devices for medium-length text
+        spotlightColumn.classList.add('col-md-6'); // 1/2 width on medium devices for medium size text
       } else {
-        spotlightColumn.classList.add('col-md-4'); // Set column to one-third width on medium devices for short text
+        spotlightColumn.classList.add('col-md-4'); // 1/3 width on medium devices for short text
       }
     }
   });
@@ -123,77 +93,194 @@ const handleSpotlight = () => {
 
 handleSpotlight();
 
+// Task 3.3
+window.onload = () => {
+  const showProgress = () => {
+    const button = document.getElementById("task3-3-button");
+    const progressBarContainer = document.querySelector(".progress");
+    const progressBar = document.getElementById("task3-3-progress-bar");
+    progressBarContainer.style.display = "none";
 
+    // Get style property in stylesheet
+    const computedStyle = window.getComputedStyle(progressBarContainer);
 
-/*
-// Task 3.1
-const align = () => {
-  const button = document.getElementById("task3-1-button");
-  const column1 = document.getElementById("task3-1-column-1"); // Updated ID
-  const column2 = document.getElementById("task-3-1-column-2");
+    const toggleProgressBar = () => {
+      // Toggle the style.display of sec-navbar 
+      if (computedStyle.display === "none") {
+        progressBarContainer.style.display = "block";
+        progressBarContainer.classList.add("sticky-top");
+        console.log("Progress bar show");
+      } else {
+        progressBarContainer.style.display = "none";
+        progressBarContainer.classList.remove("sticky-top");
+        console.log("Progress bar hide");
+      }
+    }; 
 
-  // Define an array of alignment classes
-  const alignmentClasses = ["text-start", "text-center", "text-end"];
-  let alignmentIndex = 0; // Current alignment index
+    const updateProgress = () => {
+      console.log("updating...");
+      let y_pos = window.scrollY;
+      let height = document.documentElement.scrollHeight - window.innerHeight;
+      let width = String(Math.ceil((y_pos / height) * 100));
+      let width_percent = Math.ceil((y_pos / height) * 100) + "%";
+      console.log(width_percent);
+      progressBar.style.width = width_percent;
+      progressBar.setAttribute("aria-valuenow", width);
+      console.log(progressBar.style.width); // check if width are correctly updated -> yes, but still shows empty bar on page (why?)
+      console.log(progressBar.getAttribute("aria-valuenow"));
+    };
 
-  button.addEventListener("click", () => {
-    column1.classList.remove(...alignmentClasses); // Remove all alignment classes from column 1
-    column2.classList.remove(...alignmentClasses); // Remove all alignment classes from column 2
+    button.addEventListener("click", () => {
+      console.log("click event triggered");
+      toggleProgressBar();
+      updateProgress(); // Update progress bar immediately when the button is clicked
+    });
 
-    column1.classList.add(alignmentClasses[alignmentIndex]); // Add current alignment class to column 1
-    column2.classList.add(alignmentClasses[alignmentIndex]); // Add current alignment class to column 2
+    document.addEventListener("scroll", () => {
+      console.log("scroll event triggered");
+      updateProgress();
+    });
+  };
 
-    // Increment the alignment index and wrap around if necessary
-    alignmentIndex = (alignmentIndex + 1) % alignmentClasses.length;
-  });
+  showProgress();
 };
 
-align();
-*/
-/*
-// Output content in p
-//const outputElement = document.getElementById("para")
-//console.log(outputElement.innerHTML); 
+// Task 4,5,6
+let commentID = 1002; // Starting ID for comments
 
-// Get references to the submitButton and textInput elements
-const myButton = document.getElementById("submitButton");
-const myTextInput = document.getElementById("textInput");
+// Add an event listener to load comments after the page has finished loading
+window.addEventListener('load', () => {
+  loadCommentsFromFile();
+});
 
-// Add an event listener for the click event
-myButton.addEventListener("click", function() {
+const processForm = () => {
+  // Validate email format and check for emptiness 
+  const validateEmail = () => {
+    const emailInput = document.querySelector("#new-email");
+    const emailError = document.querySelector("#new-email + .error-message");
 
-  // Get the content of the text input
-  const myTextInputContent = myTextInput.value;
-  const confirmMessage = "You inputted: " + myTextInputContent + "\nConfirm to submit?";
-  // Ask for confirmation
-  if (confirm(confirmMessage)) {
-    // Create the review box element and set its content
-    const reviewBox = document.createElement("div");
-    reviewBox.className = "reviewBox";
-    reviewBox.textContent = myTextInputContent;
-    // Add new class for the new review box
-    reviewBox.classList.add("newReviewBox");
+    if (emailInput.validity.valueMissing) {
+      emailError.textContent = "Email is required.";
+      emailInput.classList.add("is-invalid");
+      return false;
+    } else if (emailInput.validity.typeMismatch) {
+      emailError.textContent = "Please enter a valid email address.";
+      emailInput.classList.add("is-invalid");
+      return false;
+    } else {
+      emailError.textContent = "";
+      emailInput.classList.remove("is-invalid");
+      return true;
+    }
+  };
 
-    // Find the last review box element
-    //const lastReviewBox = document.querySelector(".reviewBox:last-of-type");
+  // Validate if comment is not empty 
+  const validateComment = () => {
+    const commentInput = document.querySelector("#new-comment");
+    const commentError = document.querySelector("#new-comment + .error-message");
 
-    // Find the last review box element
-    const reviewBoxes = document.querySelectorAll(".reviewBox");
-    const lastReviewBox = reviewBoxes[reviewBoxes.length - 1];
+    if (commentInput.validity.valueMissing) {
+      commentError.textContent = "Comment is required.";
+      commentInput.classList.add("is-invalid");
+      return false;
+    } else {
+      commentError.textContent = "";
+      commentInput.classList.remove("is-invalid");
+      return true;
+    }
+  };
 
-    // Append the review box after the last review box
-    lastReviewBox.insertAdjacentElement("afterend", reviewBox);
+  const isEmailValid = validateEmail();
+  const isCommentValid = validateComment();
 
-    // Clear the input field after submission
-    myTextInput.value = "";
+  if (!isEmailValid || !isCommentValid) {
+    return;
   }
-});
 
-const myButton2 = document.getElementById("colorButton");
+  // Save comment to file
+  saveCommentToFile();
 
-myButton2.addEventListener("click", function() {
-  alert("Change the color of all review boxes to blue");
-  reviewBox.css("background-color", "blue");
-});
-*/
+  // Load comments from file and update the comments section
+  loadCommentsFromFile();
+};
 
+const saveCommentToFile = () => {
+  const emailInput = document.getElementById('new-email');
+  const commentInput = document.getElementById('new-comment');
+  const colorInput = document.querySelector("input[name=new-color]:checked");
+  const email = emailInput.value;
+  const comment = commentInput.value;
+  const color = colorInput.value;
+  const text = `${email}:${comment}:${color}\n`;
+
+  // Fetch the existing file content from "hw1_file.txt"
+  fetch("hw1_file.txt")
+    .then(response => response.text())
+    .then(data => {
+      const existingContent = data;
+      const updatedContent = existingContent + text;
+
+      // Send a PUT request to update the file content with new data
+      return fetch("hw1_file.txt", {
+        method: "PUT",
+        body: updatedContent,
+        headers: {
+          'Content-Type': 'text/plain'
+        }
+      });
+    })
+    .then(response => {
+      // Check if the file was saved successfully
+      if (response.ok) {
+        // Load comments from file and update the comments section
+        loadCommentsFromFile();
+        alert("Comment saved successfully.");
+      } else {
+        alert("Error saving comment.");
+      }
+    })
+    .catch(error => {
+      console.error("Error saving comment:", error);
+    });
+};
+
+const loadCommentsFromFile = () => {
+  let contentElement = document.getElementById("comment");
+
+  // Fetch the file content from "hw1_file.txt"
+  fetch("hw1_file.txt")
+    .then(response => response.text())
+    .then(data => {
+      const emailCommentColorPairs = data.split('\n');
+      let htmlContent = '';
+
+      // Process each email, comment, and color pair
+      for (const pair of emailCommentColorPairs) {
+        if (pair !== '') {
+          const [email, comment, color] = pair.split(':');
+          const newCommentId = `c${commentID}`;
+
+          // Construct the HTML content for each pair
+          htmlContent += `<div id="${newCommentId}" class="d-flex">
+                            <div class="flex-shrink-0 circle-show">
+                              <svg height="80" width="80">
+                                <circle cx="35" cy="35" r="30" fill="${color}">
+                              </svg>
+                            </div>
+                            <div class="flex-grow-1 email-show">
+                              <h5>${email}</h5>
+                              <p>${comment}</p>
+                            </div>
+                          </div>`;
+
+          commentID++; // Increment comment ID for the next comment
+        }
+      }
+
+      // Set the generated HTML content to the comment element
+      contentElement.innerHTML = htmlContent;
+    })
+    .catch(error => {
+      console.error("Error loading comments:", error);
+    });
+}; 
